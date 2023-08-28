43241d4742002cef6f477d17b1a1af66672caefc
a <EYCHFSJD@gmail.com>
2023-08-28 21:50:05 +0800

    fix a small bug

--- src/main/java/com/keletu/genshin_weapons/Events.java
+++ src/main/java/com/keletu/genshin_weapons/Events.java
@@ -1,27 +1,28 @@
 package com.keletu.genshin_weapons;
 
 import com.keletu.genshin_weapons.network.IPlayerProvider;
-import com.keletu.genshin_weapons.network.ParticleNote;
 import com.keletu.genshin_weapons.network.PlayerVariables;
 import com.keletu.genshin_weapons.proxy.CommonProxy;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
-import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -184,8 +185,7 @@ public class Events {
 
         if(stack.getItem() == CommonProxy.SwordOfDescension && player.getEntityWorld().rand.nextInt(100) < 49 && playerVariables.ability_cd == 0)
         {
-            Vec3d vec3d = new Vec3d(player.posX, player.posY, player.posZ);
-            List<Entity> entities = player.getEntityWorld().getEntitiesWithinAABB(Entity.class, (new AxisAlignedBB(vec3d, vec3d)).grow(4.5D), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.getDistanceSqToCenter(new BlockPos(player.posX, player.posY, player.posZ)))).collect(Collectors.toList());
+            List<Entity> entities = player.getEntityWorld().getEntitiesWithinAABB(Entity.class, (new AxisAlignedBB(player.posX, player.posY, player.posZ, player.posX, player.posY, player.posZ)).grow(4.5D), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.getDistanceSqToCenter(new BlockPos(player.posX, player.posY, player.posZ)))).collect(Collectors.toList());
             for (Entity entityiterator : entities)
             {
                 if(entityiterator != player)
@@ -196,8 +196,7 @@ public class Events {
 
         if(stack.getItem() == CommonProxy.SwordOfDescension && player.getEntityWorld().rand.nextInt(100) < 49 && playerVariables.ability_cd == 0)
         {
-            Vec3d vec3d = new Vec3d(player.posX, player.posY, player.posZ);
-            List<Entity> entities = player.getEntityWorld().getEntitiesWithinAABB(Entity.class, (new AxisAlignedBB(vec3d, vec3d)).grow(4.5D), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.getDistanceSqToCenter(new BlockPos(player.posX, player.posY, player.posZ)))).collect(Collectors.toList());
+            List<Entity> entities = player.getEntityWorld().getEntitiesWithinAABB(Entity.class, (new AxisAlignedBB(player.posX, player.posY, player.posZ, player.posX, player.posY, player.posZ)).grow(4.5D), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.getDistanceSqToCenter(new BlockPos(player.posX, player.posY, player.posZ)))).collect(Collectors.toList());
             for (Entity entityiterator : entities)
             {
                 if(entityiterator != player)
@@ -240,22 +239,14 @@ public class Events {
             }
 
             if(playerVariables.the_flute_ab_stack == 5) {
-                Vec3d vec3d = new Vec3d(player.posX, player.posY, player.posZ);
-                List<Entity> entities = player.getEntityWorld().getEntitiesWithinAABB(Entity.class, (new AxisAlignedBB(vec3d, vec3d)).grow(4.5D), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.getDistanceSqToCenter(new BlockPos(player.posX, player.posY, player.posZ)))).collect(Collectors.toList());
+                List<Entity> entities = player.getEntityWorld().getEntitiesWithinAABB(Entity.class, (new AxisAlignedBB(player.posX, player.posY, player.posZ, player.posX, player.posY, player.posZ)).grow(4.5D), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.getDistanceSqToCenter(new BlockPos(player.posX, player.posY, player.posZ)))).collect(Collectors.toList());
                 for (Entity entityiterator : entities) {
                     if (entityiterator != player) {
                         entityiterator.attackEntityFrom(DamageSource.MAGIC, event.getAmount() * (CommonProxy.TheFlute.getTier(stack) + 3) / 4);
+                        player.world.playSound(null, event.getEntity().getPosition(), SoundEvents.BLOCK_NOTE_FLUTE, SoundCategory.PLAYERS, 1F, 1F);
 
-                        if(player.world.isRemote)
-                            for (int l = 0; l < 15; l++) {
-                                double d = event.getEntity().posX + (player.world.rand.nextFloat() - 0.5D) * 6.0D;
-                                double d1 = event.getEntity().posY + (player.world.rand.nextFloat() - 0.5D) * 6.0D;
-                                double d2 = event.getEntity().posZ + (player.world.rand.nextFloat() - 0.5D) * 6.0D;
-                                double d3 = (player.world.rand.nextFloat() - 0.5D) * 0.5D;
-                                double d4 = (player.world.rand.nextFloat() - 0.5D) * 0.5D;
-                                double d5 = (player.world.rand.nextFloat() - 0.5D) * 0.5D;
-                                ParticleNote entityPurpleFX = new ParticleNote(player.world, d, d1, d2, d3, d4, d5);
-                                (FMLClientHandler.instance().getClient()).effectRenderer.addEffect(entityPurpleFX);
+                        if (player.world instanceof WorldServer) {
+                            ((WorldServer) player.world).spawnParticle(EnumParticleTypes.NOTE, player.posX, player.posY, player.posZ, 6, 2.0D, 2.0D, 2.0D, 0.2D);
                         }
                     }
                 }
